8f76a70a76a785348595de5c1407f814a6c7e604
Bekah Downer <bekah2126@gmail.com>
2023-02-21 12:46:32 -0500

    Created public_header.php and populated it with head and navigation list

---
+++ private/shared/public_header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <title>Ski Resorts<abbr title="Western North Carolina">WNC <?php if(isset($page_title)) {
+      echo '- ' . $page_title; } ?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../../public/stylesheets/public.css">
+  </head>
+
+  <body>
+    <h1><?php if(isset($page_title))  echo $page_title; ?></h1>
+
+    <nav role="navigation">
+      <ul>
+        <li><a href="../public/index.php">Home</a></li>
+        <li><a href="../public/trails.php">Trails</a></li>
+        <li><a href="../public/pricing.php">Prices</a></li>
+        <li><a href="../public/gallery.php">Gallery</a></li>
+        <li><a href="../public/reviews.php">Reviews</a></li>
+      </ul>
+    </nav>
+
+  </body>
+</html>
