5177717a936e51fe5bba46f6b805196ac551da1a
Bekah Downer <bekah2126@gmail.com>
2023-03-22 15:53:24 -0400

    Fixed errors for registration and how they display. Made so that there is a default value for state that cannot be chosen.

--- public/registration.php
+++ public/registration.php
@@ -20,19 +20,21 @@ if(is_post_request()) {
   $duplicate = mysqli_query($database, "SELECT * FROM member WHERE username = '$username' or email = '$email'");
 
   if(mysqli_num_rows($duplicate) > 0) {
-    echo "Username or Email has already been taken.";
+    echo "<p id='failed'>Username or Email has already been taken.</p>"; 
+  } elseif($state_id == 'Choose State') {
+    echo "<p id='failed'>Please choose your state.</p>";
   } else {
     if($password == $confirm_password) {
       $sql = "INSERT INTO member (username, first_name, last_name, email, state_id, hashed_password) VALUES ('$username', '$first_name', '$last_name', '$email', '$state_id', '$hashed_password')";
       mysqli_query($database, $sql);
-      var_dump($sql);
-      redirect_to(url_for("public/index.php"));
+      redirect_to(url_for("public/login.php"));
 
     } else {
-      echo "Passwords do not match.";
+      echo "<p id='failed'>Passwords do not match.</p>";
     }
   }
 }
+
 
 ?>
 
@@ -41,22 +43,17 @@ if(is_post_request()) {
   <h2>Create an Account</h2>
   <p>*Please fill out all fields.</p>
   <form action="" method="post">
-    <!-- <label for="fname">*First Name:</label> -->
-    <input type="text" name="fname" id="fname" value="" placeholder="First Name" required><br>
-    <!-- <label for="lname">*Last Name:</label> -->
-    <input type="text" name="lname" id="lname" value="" placeholder="Last Name" required><br>
-    <!-- <label for="uname">*Username:</label> -->
-    <input type="text" name="uname" id="uname" value="" placeholder="Username" required><br>
-    <!-- <label for="password">*Password:</label> -->
-    <input type="password" name="password" id="password" placeholder="Password" required><br>
-    <!-- <label for="confirm_password">*Confirm Password:</label> -->
-    <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" required><br>
-    <!-- <label for="email">*Email</label> -->
-    <input type="email" name="email" id="email" value="" placeholder="Email" required><br>
+    <input type="text" name="fname" id="fname" value="<?php if (isset($first_name)) echo $first_name; ?>" placeholder="First Name" required><br>
+    <input type="text" name="lname" id="lname" value="<?php if (isset($last_name)) echo $last_name; ?>" placeholder="Last Name" required><br>
+    <input type="text" name="uname" id="uname" value="<?php if (isset($username)) echo $username; ?>" placeholder="Username" required><br>
+    <input type="password" name="password" id="password" value="<?php if (isset($password)) echo $password; ?>" placeholder="Password" required><br>
+    <input type="password" name="confirm_password" id="confirm_password" value="<?php if (isset($confirm_password)) echo $confirm_password; ?>" placeholder="Confirm Password" required><br>
+    <input type="email" name="email" id="email" value="<?php if (isset($email)) echo $email; ?>" placeholder="Email" required><br>
     <label for="state">*State:</label>
     <?php $sql = "SELECT state_id, state_abbreviation FROM state ";
     if($r_set=$database->query($sql)) {
-      echo "<select name=state id=state required>";
+      echo "<select name=state id=state>";
+      echo "<option selected>Choose State</option>";
       while($row=$r_set->fetch_assoc()) {
         echo "<option value=$row[state_id]>$row[state_abbreviation]</option>";
       }
